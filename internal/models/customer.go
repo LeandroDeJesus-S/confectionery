d1a64af275effbd379ca28469523b3268d342f77
LeandroDeJesus-S <jstleandro@gmail.com>
2025-06-03 00:06:20 -0300

    fix customer fname and lname fields size

--- internal/models/customer.go
+++ internal/models/customer.go
@@ -2,9 +2,9 @@ package models
 
 type Customer struct {
 	ID       uint
-	Fname    string
-	Lname    string
-	Email    string `gorm:"unique"`
+	Fname    string `gorm:"size:100;not null"`
+	Lname    string `gorm:"size:255;not null"`
+	Email    string `gorm:"unique;not null;size:345"`
 	Active   bool   `gorm:"default:true"`
 	Orders []Order  `gorm:"many2many:orders;"`
 }
